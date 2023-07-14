ce6c5c7a3254b96bdff35f6c91526a8654372b4b
Francisco Vega <francisco.vega@aspi.cl>
2023-07-14 13:10:54 -0400

    Updates

---
+++ precache-manifest.8d0dc5e7e7471c0671a3458904f3b3fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1033b5a8a5701f65c9bc45395e1d0bc4",
+    "url": "/Army-Memo-Maker/index.html"
+  },
+  {
+    "revision": "44313d7192560c5eb3e6",
+    "url": "/Army-Memo-Maker/static/css/main.a443cab5.chunk.css"
+  },
+  {
+    "revision": "d6e8c5d9f2f12b1c7d6d",
+    "url": "/Army-Memo-Maker/static/js/2.a907aa84.chunk.js"
+  },
+  {
+    "revision": "ec69943f217f4e265f3b95d5ad7c2bda",
+    "url": "/Army-Memo-Maker/static/js/2.a907aa84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44313d7192560c5eb3e6",
+    "url": "/Army-Memo-Maker/static/js/main.64bc4707.chunk.js"
+  },
+  {
+    "revision": "bffd768b82dea285499a",
+    "url": "/Army-Memo-Maker/static/js/runtime-main.97cb403a.js"
+  },
+  {
+    "revision": "c4c16d8a7226dadca83937346bdfa0e5",
+    "url": "/Army-Memo-Maker/static/media/DoD Seal.c4c16d8a.PNG"
+  },
+  {
+    "revision": "de0508ced3965ef598ee797c7df3f193",
+    "url": "/Army-Memo-Maker/static/media/attnFromExample.de0508ce.JPG"
+  }
+]);
